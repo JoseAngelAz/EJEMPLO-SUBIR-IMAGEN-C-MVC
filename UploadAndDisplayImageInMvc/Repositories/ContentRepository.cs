430ebb523c4dcf96b99e46e6bfb82bd58a6dcaee
Tania Flamenco <Tania.Flamenco@sv.cass.int>
2021-02-24 11:58:03 -0600

    subiendo first

---
+++ UploadAndDisplayImageInMvc/Repositories/ContentRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using UploadAndDisplayImageInMvc.Models;
+using UploadAndDisplayImageInMvc.ViewModel;
+
+namespace UploadAndDisplayImageInMvc.Repositories
+{
+    public class ContentRepository
+    {
+        private readonly DBContext db = new DBContext();
+        public async Task<int> UploadImageInDataBase(HttpPostedFileBase file, ContentViewModel contentViewModel)
+        {
+            contentViewModel.Image = ConvertToBytes(file);
+            var Content = new  Content 
+            {
+                Title = contentViewModel.Title,
+                Description = contentViewModel.Description,
+                Contents = contentViewModel.Contents,
+                Image = contentViewModel.Image
+            };
+            db.Contents.Add(Content);
+            int i = db.SaveChanges();
+            if (i == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public byte[] ConvertToBytes(HttpPostedFileBase image)
+        {
+            byte[] imageBytes = null;
+            BinaryReader reader = new BinaryReader(image.InputStream);
+            imageBytes = reader.ReadBytes((int)image.ContentLength);
+            return imageBytes;
+        }
+    }
+}
